d51e6e204991a670c5cca7f30eb98b565ea25f53
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-04-11 21:43:16 +0300

    11/04/21

--- EMPLOYEE.sql
+++ EMPLOYEE.sql
@@ -10,18 +10,22 @@ CREATE SCHEMA company;
  );
  
  DESCRIBE employee;
- INSERT INTO employee (employee_name, employee_age, department_id) VALUES ('Ivan', 21, 1);
- SELECT employee_name AS name, employee_department as department FROM employee;
+ INSERT INTO employee (employee_name, employee_age, department_id) VALUES ('Vasya', 29, 4);
+ SELECT employee_id AS ID, employee_name AS name, department_id as department FROM employee;
+-- UPDATE employee SET department_id = 4 WHERE employee_id = 4;
 
  CREATE TABLE IF NOT EXISTS department(
  department_id INT UNIQUE NOT NULL AUTO_INCREMENT,
  department_name VARCHAR(20) NOT NULL
  );
 
-INSERT INTO department(department_name) VALUES ('Developer');
-UPDATE department SET department_name = 'DB admin' WHERE department_ID = 2;
+INSERT INTO department(department_name) VALUES ('HR');
+UPDATE department SET department_name = 'DB admin'
+WHERE department_ID = 2;
+
 SELECT * FROM department;
 
+-- Взаємний зв'язок між таблицями (employee --> department)
 ALTER TABLE employee -- змінити таблицю employee
 ADD CONSTRAINT fk_employee_on_dept -- додається обмеження на fk_employee_on_dept
 FOREIGN KEY (department_id) -- створення злвнішнього ключа
@@ -35,3 +39,15 @@ employee_age AS age,
 department.department_name AS department 
 FROM employee
 INNER JOIN department ON department.department_id = employee.department_id  ORDER BY employee.employee_id ASC;
+
+-- Взаємний зв'язок між таблицями (department --> employee)
+ALTER TABLE department ADD COLUMN mgr_employee_id INT; -- додається стовпець mgr_employee_id у department для взаємного зв'язку
+
+ALTER TABLE department
+ADD CONSTRAINT fk_department_on_empl_mgl -- додається обмеження fk_department_on_empl_mgl
+FOREIGN KEY (mgr_employee_id) -- зовнішній ключ mgr_employee_id
+REFERENCES employee (employee_id); -- прив'язка до стовпця {employee_id} у employee
+
+SELECT * FROM company.department;
+SELECT * FROM company.employee;
+
